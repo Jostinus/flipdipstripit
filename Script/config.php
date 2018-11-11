1d45142f5e14173949ab24512afb866707020a82
dick <jostclaassen@Project2.local>
2018-11-11 20:57:58 +1300

    saz
    z

---
+++ Script/config.php
@@ -0,0 +1,26 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+header("Location: ./install");
+// MySQL Hostname
+$sql_db_host = "127.0.0.1";
+// MySQL Database User
+$sql_db_user = "617b8b865a8e";
+// MySQL Database Password
+$sql_db_pass = "32fdfffe5fb45b2d";
+// MySQL Database Name
+$sql_db_name = "Flipphone24";
+
+// Site URL
+$site_url = "http://http://motiv8ion.tv"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = ""; // Your purchase code, don't give it to anyone. 
+?>
